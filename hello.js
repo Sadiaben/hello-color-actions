f0895849b92d89d9d846ea8ed53b1989d8e11f9a
Sadia <86654310+Globeservices@users.noreply.github.com>
2023-10-30 21:41:45 +0100

    modif hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal,I am sadia!";
+const hello = () => "Hello World from the Office Hours in the terminal,I am sadia yes !";
 console.log(hello());
 exports.hello = hello;
